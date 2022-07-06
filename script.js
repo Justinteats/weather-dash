a591c1e2974ff101b48d9b1b22828bacfab198d1
Justin Teats <justinteats@Justins-MacBook-Air.local>
2022-07-05 20:24:54 -0700

    restart on script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var searchHistoryContainerEl = document.querySelector(
   "#search-history-container"
 );
 var rootUrl = "https://api.openweathermap.org";
-var weatherApiKey = "05de828319fc1cf4cf97ed816ea2b0fc";
+var weatherApiKey = "f106647a1a76ba0d309cf79f5bb351b7";
 var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
 
 var repoContainerEl = document.querySelector("#search-history-container"); 
@@ -45,79 +45,3 @@ var buildSearchHistoryItems = function () {
   }
 };
 
-var updateSearchHistory = function (searchData) {
-  var newSearchHistoryItem = {
-    city: searchData.name,
-    lat: searchData.lat,
-    lon: searchData.lon, 
-  };
-
-  if (searchHistory === null) {
-    searchHistory = [];
-  }
-
-  searchHistory.push(newSearchHistoryItem);
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-
-};
-
-var getCoordinatesByCityName = function (cityName) {
-
-  var limit = 5;
-  var lat = null;
-  var lon = null;
-  var geoUrl = `${rootUrl}/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${weatherApiKey}`;
-
-  // make a request to the url
-  fetch(geoUrl).then(function (response) {
-    response.json().then(function (data) {
-      
-      console.log(data[0]); 
-      lat = data[0].lat;
-      lon = data[0].lon;
-      getForecastData(lat, lon);
-      updateSearchHistory(data[0]);
-    });
-  });
-}; 
-
-var formSubmitHandler = function (event) {
-  event.preventDefault();
- 
-  var cityName = nameInputEl.value.trim();
-  if (cityName) {
-    getCoordinatesByCityName(cityName); 
-    nameInputEl.value = ""; 
-  } else {
-    alert("Please enter a city"); 
-  }
-  console.log(event);
-};
-
-var displayRepos = function (repos) {
-  console.log(repos);
-
-  repoContainerEl.textContent = ""; 
-
-  for (var i = 0; i < repos.length; i++) {
-   
-
-    var repoName = repos[i].owner.login + "/" + repos[i].name;
-    var repoEl = document.createElement("div");                      
-    repoEl.classList = "list-item flex-row justify-space-between align-center"; 
-
-    var titleEl = document.createElement("span"); 
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl); 
-
-    repoContainerEl.appendChild(repoEl);
-};
-
-userFormEl.addEventListener("submit", formSubmitHandler);
-
-var pageLoad = function () {
-  buildSearchHistoryItems();
-};
-
-pageLoad()};
